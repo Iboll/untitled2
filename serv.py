52bfa26455c8d5f20c97474451aee5a06c87dca6
Soltan <soltan.shamgunov@yandex.ru>
2020-03-17 23:11:27 +0300

    sqlalchemy2

--- serv.py
+++ serv.py
@@ -1,66 +1,101 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
+from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import PasswordField, BooleanField, SubmitField, StringField, TextAreaField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
 from data import db_session
 from data.news import Jobs
 from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-db_session.global_init("db/blogs.sqlite")
+db_session.global_init("db/mars_explorer.sqlite")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route("/")
 def index():
     session = db_session.create_session()
-    news = session.query(Jobs).filter(Jobs.is_private != True)
-    return render_template("index.html", news=news)
+    jobs = session.query(Jobs).filter(Jobs.is_finished is False)
+    return render_template("index.html", news=jobs)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    about = TextAreaField("Немного о себе")
+    submit = SubmitField('Войти')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 def main():
-    session = db_session.create_session()
-    user = User()
-    user.surname = "Scott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = "captain"
-    user.speciality = "research engineer"
-    user.address = "module_1"
-    user.email = "scott_chief@mars.org"
-    session.add(user)
-    session.commit()
-    session = db_session.create_session()
-    user = User()
-    user.surname = "Martin"
-    user.name = "Iden"
-    user.age = 23
-    user.position = "engineer"
-    user.speciality = "research engineer"
-    user.address = "module_2"
-    user.email = "asd@mars.org"
-    session.add(user)
-    session.commit()
-    session = db_session.create_session()
-    user = User()
-    user.surname = "Mark"
-    user.name = "Jacobs"
-    user.age = 19
-    user.position = "second captain"
-    user.speciality = "research engineer"
-    user.address = "module_1"
-    user.email = "jacobs@mars.org"
-    session.add(user)
-    session.commit()
-    session = db_session.create_session()
-    user = User()
-    user.surname = "Scott"
-    user.name = "Sanders"
-    user.age = 21
-    user.position = "second engineer"
-    user.speciality = "research engineer"
-    user.address = "module_3"
-    user.email = "rt@mars.org"
-    session.add(user)
-    session.commit()
-    #app.run()
+    app.run()
 
 
 if __name__ == '__main__':
